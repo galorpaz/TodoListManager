394682c657c1a98ff6a391f8b58fb64a1f23e105
galor paz <galor.paz@mail.huji.ac.il>
2015-04-12 18:58:37 +0300

    fixed details according to exercise definition

---
+++ app/src/main/java/com/example/gali/todolistmanager/Todo_item.java
@@ -0,0 +1,14 @@
+package com.example.gali.todolistmanager;
+
+/**
+ * Created by gali on 10-Apr-15.
+ */
+public class Todo_item {
+    public String task;
+    public String date;
+
+    public Todo_item(String new_task, String new_date){
+        this.task = new_task;
+        this.date = new_date;
+    }
+}
